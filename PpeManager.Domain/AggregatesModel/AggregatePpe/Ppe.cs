39900d6a53a100f7cf47f0273b19f493f31ffad3
Igor de Brito Coura <igor-coura@hotmail.com>
2021-12-18 22:25:01 -0300

    Created Ppe

---
+++ PpeManager.Domain/AggregatesModel/AggregatePpe/Ppe.cs
@@ -0,0 +1,23 @@
+﻿namespace PpeManager.Domain.AggregatesModel.AggregatePpe
+{
+    public class Ppe: Entity, IAggregateRoot
+    {
+        public Name Name { get; }
+        public Description Description { get; }
+        public Ppe(Name name, Description description)
+        {
+            AddNotifications(
+                name.contract,
+                description.contract
+                );
+            if (IsValid)
+            {
+                Name = name;
+                Description = description;
+            }
+            
+        }
+
+        
+    }
+}
