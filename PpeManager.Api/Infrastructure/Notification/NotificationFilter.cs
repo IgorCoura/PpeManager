3b995e427bf91341a85275a79d136ec5a6ffe4eb
Igor de Brito Coura <igor-coura@hotmail.com>
2021-12-18 22:31:11 -0300

    Created notification system

---
+++ PpeManager.Api/Infrastructure/Notification/NotificationFilter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace PpeManager.Api.Infrastructure
+{
+    public class NotificationFilter : IAsyncResultFilter
+    {
+        private readonly NotificationContext _notificationContext;
+
+        public NotificationFilter(NotificationContext notificationContext)
+        {
+            _notificationContext = notificationContext;
+        }
+        public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            if (!_notificationContext.IsValid)
+            {
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.HttpContext.Response.ContentType = "application/json";
+
+                var notifications = JsonConvert.SerializeObject(_notificationContext.Notifications.Select(s => s.Message));
+                await context.HttpContext.Response.WriteAsync(notifications);
+
+                return;
+            }
+            await next();
+        }
+    }
+}
