e5a12ebdec26b01441af362b1e31ab05f6c3e1e9
Igor de Brito Coura <igor-coura@hotmail.com>
2021-12-18 22:22:17 -0300

    Created ppeDTO

---
+++ PpeManager.Api/Application/DTO/CreatePpeDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace PpeManager.Api.DTO
+{
+    public record CreatePpeDTO
+    {
+        public String Name { get; }
+        public String Description { get; }
+        public CreatePpeDTO(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+
+    }
+}
---
+++ PpeManager.Api/Application/DTO/PpeDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace PpeManager.Api.Application.DTO
+{
+    public record PpeDTO
+    {
+        public int Id { get; }
+        public String Name { get; }
+        public String Description { get; }
+
+        public PpeDTO(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+
+    }
+}
