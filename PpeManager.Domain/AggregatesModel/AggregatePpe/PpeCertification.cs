c4e118c2dba9a5ea1f535a3545a7720be7a595ba
Igor de Brito Coura <igor-coura@hotmail.com>
2021-12-18 22:27:56 -0300

    Created PpeCertification

---
+++ PpeManager.Domain/AggregatesModel/AggregatePpe/PpeCertification.cs
@@ -0,0 +1,21 @@
+﻿namespace PpeManager.Domain.AggregatesModel.AggregatePpe
+{
+    public class PpeCertification: Entity
+    {
+        public PpeCertification(Ppe ppe, string approvalCertificateNumber, DateTime validity, int durability)
+        {
+            Ppe = ppe;
+            ApprovalCertificateNumber = approvalCertificateNumber;
+            Validity = validity;
+            Durability = durability;
+        }
+
+        public Ppe Ppe { get; private set; }
+        public String ApprovalCertificateNumber { get; private set; }
+        public DateTime Validity { get; private set; }
+        public int Durability { get; private set; }
+
+
+
+    }
+}
