1aedb97edc17d8f82d064cba422923c56143d96c
Igor de Brito Coura <igor-coura@hotmail.com>
2021-12-18 22:19:26 -0300

    create filter for duplicates commands

---
+++ PpeManager.Api/Application/Commands/IdentifiedCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace PpeManager.Api.Application.Commands
+{
+    public class IdentifiedCommand<T, R>: IRequest<R> where T: IRequest<R>
+    {
+        public T Command { get; private set; }
+        public Guid Id { get; private set; }
+
+
+        public IdentifiedCommand(T command, Guid id)
+        {
+            Command = command;
+            Id = id;
+        }
+    }
+}
---
+++ PpeManager.Api/Application/Commands/IdentifiedCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿namespace PpeManager.Api.Application.Commands
+{
+    public class IdentifiedCommandHandler<T, R> : IRequestHandler<IdentifiedCommand<T, R>, R> where T : IRequest<R>
+    {
+        private readonly IMediator _mediator;
+        private readonly IRequestManager _requestManager;
+
+        public IdentifiedCommandHandler(IMediator mediator, IRequestManager requestManager)
+        {
+            _mediator = mediator;
+            _requestManager = requestManager;
+        }
+
+
+        /// <summary>
+        /// This method handles the command. It just ensures that no other request exists with the same ID, and if this is the case
+        /// just enqueues the original inner command.
+        /// </summary>
+        /// <param name="message">IdentifiedCommand which contains both original command & request ID</param>
+        /// <returns>Return value of inner command or default value if request same ID was found</returns>
+        public async Task<R> Handle(IdentifiedCommand<T, R> request, CancellationToken cancellationToken)
+        {
+            var alreadyExists = await _requestManager.ExistAsync(request.Id);
+            if (!alreadyExists)
+            {
+                throw new DuplicateCommandException(nameof(request.Command)+" duplicate");
+            }
+            else
+            {
+                var manager = _requestManager.CreateRequestForCommandAsync<T>(request.Id);
+
+                var result = await _mediator.Send(request.Command, cancellationToken);
+
+                manager.Wait();
+
+                return result;
+            }
+
+            
+        }
+    }
+}
