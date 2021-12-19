938eba6c9b08ee8d1f5b2ca7ac83607167266b9a
Igor de Brito Coura <igor-coura@hotmail.com>
2021-12-18 22:24:29 -0300

    Created container injector

---
+++ PpeManager.Api/Infrastructure/AutofacModules/ApplicationModule.cs
@@ -0,0 +1,22 @@
+﻿namespace PpeManager.Api.Infrastructure.AutofacModules;
+
+public class ApplicationModule: Autofac.Module
+{
+
+    public ApplicationModule()
+    {
+
+    }
+
+    protected override void Load(ContainerBuilder builder)
+    {
+
+        builder.RegisterType<RequestManager>()
+            .As<IRequestManager>()
+            .InstancePerLifetimeScope();
+
+        builder.RegisterType<NotificationContext>()
+            .InstancePerLifetimeScope();
+
+    }
+}
---
+++ PpeManager.Api/Infrastructure/AutofacModules/MediatorModule.cs
@@ -0,0 +1,25 @@
+﻿namespace PpeManager.Api.Infrastructure.AutofacModules;
+
+public class MediatorModule : Autofac.Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterAssemblyTypes(typeof(IMediator).GetTypeInfo().Assembly)
+            .AsImplementedInterfaces();
+
+        // Register all the Command classes (they implement IRequestHandler) in assembly holding the Commands
+        builder.RegisterAssemblyTypes(typeof(CreatePpeCommand).GetTypeInfo().Assembly)
+            .AsClosedTypesOf(typeof(IRequestHandler<,>));
+
+        
+
+
+        builder.Register<ServiceFactory>(context =>
+        {
+            var componentContext = context.Resolve<IComponentContext>();
+            return t => { object o; return componentContext.TryResolve(t, out o) ? o : null; };
+        });
+
+
+    }
+}
