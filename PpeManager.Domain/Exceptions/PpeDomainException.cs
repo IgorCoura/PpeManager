a62e0b51544336338e4d6af25fe963a7cfebd472
Igor de Brito Coura <igor-coura@hotmail.com>
2021-12-18 22:28:12 -0300

    Created Ppe Exception

---
+++ PpeManager.Domain/Exceptions/PpeDomainException.cs
@@ -0,0 +1,11 @@
+﻿namespace PpeManager.Domain.Exceptions
+{
+    public class PpeDomainException: Exception
+    {
+        public PpeDomainException() { }
+
+        public PpeDomainException(string message): base(message) { }
+
+        public PpeDomainException(string message, Exception innerException): base(message, innerException) { }
+    }
+}
